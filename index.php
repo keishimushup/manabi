3d90cc914febfce57d7f88f5c9503c164c5d76ec
keishimushup <108566690+keishimushup@users.noreply.github.com>
2022-11-17 11:38:34 +0000

    10 配列

--- index.php
+++ index.php
@@ -9,29 +9,15 @@
 
   <?php
 
-    // 変数$numを定義し、好きな数字を代入してください
-    $num = 19;
-
-    // 変数$remainderを定義し、変数$numを3で割った時の余りを代入してください
-    $remainder = $num % 3;
-
-    // switch文を用いてください
-    switch ($remainder) {
-      case 0:
-        echo '大吉です。';
-        break;
-      case 1:
-        echo '中吉です。';
-        break;
-      case 2:
-        echo '小吉です。';
-        break;
-      default:
-        echo '凶です。';
-        break;
-
-    }
-
+    // この下に配列を作ってください
+    $colors = array('赤', '青', '黄');
+    
+    echo $colors[0];
+    
+    $colors[] = '白';
+    
+    echo $colors[3];
+    
   ?>
 
 </body>
