d121b4cb576bb8352c602d5df4dfafbb10de745c
keishimushup <108566690+keishimushup@users.noreply.github.com>
2022-11-17 11:49:18 +0000

    11 連想配列

--- index.php
+++ index.php
@@ -9,15 +9,13 @@
 
   <?php
 
-    // この下に配列を作ってください
-    $colors = array('赤', '青', '黄');
-    
-    echo $colors[0];
-    
-    $colors[] = '白';
-    
-    echo $colors[3];
-    
+    // この下に連想配列を作ってください
+    $scores = array('数学' => 70, '英語' => 90, '国語' => 80);
+
+    $scores['国語'] += 5;
+
+    echo $scores['国語'];
+
   ?>
 
 </body>
