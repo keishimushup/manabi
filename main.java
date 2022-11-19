6af1278cab0cffe2cd0743e03cbca92cb526dc90
keishimushup <108566690+keishimushup@users.noreply.github.com>
2022-11-19 07:48:47 +0000

    比較演算2

--- main.java
+++ main.java
@@ -1,22 +1,10 @@
 class Main {
   public static void main(String[] args) {
-    // 「true」を用いて、「真」を表す真偽値を出力してください
-    System.out.println(true);
+    // 「4 + 2」と「6」を比較し、falseとなるようにしてください
+    System.out.println(4 + 2 > 6);
 
-    // 「false」を用いて、「偽」を表す真偽値を出力してください
-    System.out.println(false);
-
-    // 「==」を用いて、値を比較した結果を出力してください
-    System.out.println(12 / 4 == 3);
-
-    // 「!=」を用いて、値を比較した結果を出力してください
-    System.out.println(12 / 4 != 3);
-
-    // 変数を定義し、値を比較した結果を代入してください
-    boolean bool = 3 * 9 == 27;
-
-    // 変数boolの値を出力してください
-    System.out.println(bool);
+    // 「4 + 2」と「6」を比較し、trueとなるようにしてください
+    System.out.println(4 + 2 >= 6);
 
   }
 }
