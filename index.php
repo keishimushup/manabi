a6d869156f18b9a809208370027bb7bcc99cd5a4
keishimushup <108566690+keishimushup@users.noreply.github.com>
2022-11-21 12:45:03 +0000

    15continue文

--- index.php
+++ index.php
@@ -10,9 +10,9 @@
   <?php
 
     // この下にfor文を書いてください
-    for ($i = 1; $i <= 1000; $i++){
-      if ($i > 500){
-        break;
+    for ($i = 1; $i <= 1000; $i++) {
+      if ($i % 3 == 0) {
+        continue;
       }
       echo $i;
       echo '<br>';
