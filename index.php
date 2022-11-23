6dfded365bc79aaee89cca1aeba393630ffa2f05
keishimushup <108566690+keishimushup@users.noreply.github.com>
2022-11-23 02:12:45 +0000

    18 関数の自作

--- index.php
+++ index.php
@@ -9,22 +9,22 @@
 
   <?php
 
-    $str = 'progate';
+    // 関数helloを定義してください
+    function hello() {
+      echo 'Hello, world!';
+    }
 
-    // strlenを用いて$strの長さをechoしてください
-    echo strlen($str);
-
-    echo '<br>';
-
-    $array = array('HTML', 'CSS', 'PHP');
-
-    // countを用いて$arrayの要素数をechoしてください
-    echo count($array);
+    // 関数helloを呼び出してください
+    hello();
 
     echo '<br>';
+    // 関数printRectangleAreaを定義してください
+    function printRectangleArea($height, $width) {
+      echo $height * $width;
+    }
 
-    // randを用いて10から15までのランダムな数字をechoしてください
-    echo rand(10, 15);
+    // 引数を(5, 10)としてprintRectangleAreaを呼び出してください
+    printRectangleArea(5, 10);
 
   ?>
 
