a8ea5ddb59b23fb24d6e579e13cb4f00d7b449d6
keishimushup <108566690+keishimushup@users.noreply.github.com>
2022-11-26 12:03:07 +0000

    1 メソッド使用

--- main.java
+++ main.java
@@ -1,21 +1,10 @@
 class Main {
   public static void main(String[] args) {
-    // 変数numbersに、与えられた数字の配列を代入してください
-    int[] numbers = {1, 4, 6, 9, 13, 16};
+    hello();
+  }
 
-    int oddSum = 0;
-    int evenSum = 0;
-
-    // for文を用いて、配列numbersの偶数の和と奇数の和を求めてください
-    for (int number : numbers) {
-      if (number % 2 == 0) {
-        evenSum += number;
-      } else {
-        oddSum += number;
-      }
-    }
-
-    System.out.println("奇数の和は" + oddSum + "です");
-    System.out.println("偶数の和は" + evenSum + "です");
+  public static void hello() {
+    // "Hello World"を、"Hello Java"に書き換えてください
+    System.out.println("Hello Java");
   }
 }
