ebf0280e96ae5492ed1daa62f1d744d76cfa5981
keishimushup <108566690+keishimushup@users.noreply.github.com>
2022-11-29 08:49:47 +0000

    18 switch

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-const age = 24;
+const n = 2;
 
-// 指定された条件のif文を作成してください
-if (age >= 20 && age < 30) {
-  console.log("私は20代です");
+switch (n) {
+  case 1:
+    console.log("大吉です");
+    break;
+
+  // nの値が2のcaseを追加してください
+  case 2:
+    console.log("吉です");
+    break;
+  
+  
+  
+  // nの値が3のcaseを追加してください
+  case 3:
+    console.log("小吉です");
+    break;
+  
+  
 }
