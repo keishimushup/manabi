3146a938fdd5a0b3c423398aec9d7c0f75819849
keishimushup <108566690+keishimushup@users.noreply.github.com>
2022-12-02 12:37:37 +0000

    10 変数の更新2

--- index.rb
+++ index.rb
@@ -2,10 +2,10 @@ length = 9
 width = 8
 puts width
 puts length * width
-puts "----"
 
-# 変数widthの値を13に変更してください
-width = 13
+puts "----"
+# widthの値に5を足して、widthの値を変更してください
+width += 5
 
 puts width
 puts length * width
