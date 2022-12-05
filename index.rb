0330a2ac0e77fcfbcc5ba2cb990b0f058a905ef3
keishimushup <108566690+keishimushup@users.noreply.github.com>
2022-12-05 11:45:47 +0000

    13 比較演算子2

--- index.rb
+++ index.rb
@@ -1,11 +1,11 @@
-score = 80
+score = 100
 
-# 「score > 80」を出力してください
-puts score > 80
-# 「score <= 80」を出力してください
-puts score <= 80
+# scoreの値が100の場合、「満点です」と出力してください
+if score == 100
+  puts "満点です"
+end
 
-# scoreの値が80以下の場合に、「がんばりましょう」と出力してください
-if score <= 80
-  puts "がんばりましょう"
+# scoreの値が100でない場合、「満点ではありません」と出力してください
+if score != 100
+  puts "満点ではありません"
 end
