a331089c93e470a21d500531e9b9ae6e1be3f87c
keishimushup <108566690+keishimushup@users.noreply.github.com>
2022-12-07 08:00:25 +0000

    6 ハッシュの使用

--- index.rb
+++ index.rb
@@ -1,6 +1,16 @@
-# 変数examにハッシュを代入してください
 exam = {"subject" => "Math", "score" => 80}
-# （メモ）キーに文字列を使うときは""が必要になる
 
-# 変数examを出力してください
-puts exam
+# キー「"subject"」の値を出力してください
+puts exam["subject"]
+
+# キー「"subject"」の値を「"Science"」に更新してください
+exam["subject"] = "Science"
+
+# もう一度、キー「"subject"」の値を出力してください
+puts exam["subject"]
+
+# キーが「"grade"」、値が「"good"」の要素を追加してください
+exam["grade"] = "good"
+
+# キー「"grade"」の値を出力してください
+puts exam["grade"]
