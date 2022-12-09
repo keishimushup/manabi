6d3252267c6c524f8a2541ebea1e7eace8c3585f
keishimushup <108566690+keishimushup@users.noreply.github.com>
2022-12-09 05:08:30 +0000

    1 メソッドとは

--- index.rb
+++ index.rb
@@ -1,19 +1,10 @@
-characters = [
-  {name: "にんじゃわんこ", age: 14},
-  {name: "ひつじ仙人"},
-  {name: "ベイビーわんこ", age: 5},
-  {name: "とりずきん"}
-]
-
-characters.each do |character|
-  puts "--------------------"
-  puts "名前は#{character[:name]}です"
-  
-  # キー:ageの値に応じて年齢の情報を出力してください
-  if character[:age]
-    puts "年齢は#{character[:age]}歳です"
-  else
-    puts "年齢は秘密です"
-  end
-  
+def introduce
+  puts "こんにちは"
+  puts "私はにんじゃわんこです"
+  # 出力を追加してください
+  puts "14歳です"
 end
+
+puts "-----自己紹介-----"
+# introduceメソッドを呼び出してください
+introduce
