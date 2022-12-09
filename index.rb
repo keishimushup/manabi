3d4fc4bf1b1f894dbb8ea210d9e9ad57b709443f
keishimushup <108566690+keishimushup@users.noreply.github.com>
2022-12-09 05:43:53 +0000

    8 様々な戻り値

--- index.rb
+++ index.rb
@@ -1,12 +1,12 @@
-def discount(price)
-# 「price / 2」を戻り値として返してください
-	return price / 2
+# shipping_free?メソッドを定義してください
+def shipping_free?(price)
+  return price >= 5000
 end
 
-puts "テレビがセール中です！"
 
-# discountメソッドを呼び出し、その戻り値を変数half_priceに代入してください
-half_price = discount(15000)
-
-# 変数half_priceを用いて、「特別価格で〇〇円です」となるように出力してください
-puts "特別価格で#{half_price}円です"
+# if文の条件式でshipping_free?メソッドを呼び出してください
+if shipping_free?(3000)
+	puts "5000円以上のお買い上げなので送料はいただきません"
+else
+	puts "追加で送料をいただきます"
+end
