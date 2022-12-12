95bec3c5cd62a1eb2d9d5acff4df465b510d200c
keishimushup <108566690+keishimushup@users.noreply.github.com>
2022-12-12 07:28:19 +0000

    11 initialize

--- index.rb
+++ index.rb
@@ -2,11 +2,16 @@ class Menu
   attr_accessor :name
   attr_accessor :price
   
+  # initializeメソッドを定義してください
+  def initialize
+    self.name = "ピザ"
+    self.price = 800
+  end
+  
   def info
     return "#{self.name} #{self.price}円"
   end
   
-  # get_total_priceメソッドを定義してください
   def get_total_price(count)
     total_price = self.price * count
     if count >= 3
@@ -14,12 +19,9 @@ class Menu
     end
     return total_price
   end
-  
 end
 
 menu1 = Menu.new
-menu1.name = "ピザ"
-menu1.price = 800
 
-# menu1に対してget_total_priceメソッドを呼び出してください
-puts menu1.get_total_price(3)
+# menu1に対してinfoメソッドを呼び出して戻り値を出力してください
+puts menu1.info
