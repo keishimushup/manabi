824caa9c84e8512a169872a5237d719674b72928
keishimushup <108566690+keishimushup@users.noreply.github.com>
2022-12-12 07:42:19 +0000

    14 メニュー一覧

--- index.rb
+++ index.rb
@@ -1,5 +1,15 @@
 require "./menu"
-# 引数を渡してインスタンスを生成してください
-menu1 = Menu.new(name: "すし", price: 1000)
 
-puts menu1.info
+menu1 = Menu.new(name: "ピザ", price: 800)
+menu2 = Menu.new(name: "すし", price: 1000)
+menu3 = Menu.new(name: "コーラ", price: 300)
+menu4 = Menu.new(name: "お茶", price: 200)
+
+# 変数menusを定義して配列を代入してください
+menus = [menu1, menu2, menu3, menu4]
+
+# menusに対して繰り返し処理を実行してください
+
+menus.each do |menu|
+  puts menu.info
+end
