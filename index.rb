09f0d563745e39e4f5dc5b560d5565d509fc4d47
keishimushup <108566690+keishimushup@users.noreply.github.com>
2022-12-12 08:02:32 +0000

    16 メニューの選択

--- index.rb
+++ index.rb
@@ -5,11 +5,30 @@ menu2 = Menu.new(name: "すし", price: 1000)
 menu3 = Menu.new(name: "コーラ", price: 300)
 menu4 = Menu.new(name: "お茶", price: 200)
 
-# 変数menusを定義して配列を代入してください
 menus = [menu1, menu2, menu3, menu4]
 
-# menusに対して繰り返し処理を実行してください
-
+index = 0
 menus.each do |menu|
-  puts menu.info
+  puts "#{index}. #{menu.info}"
+  index += 1
 end
+
+puts "--------------"
+puts "メニューの番号を選択してください"
+
+# 入力を数値として受け取って変数orderに代入してください
+order = gets.chomp.to_i
+
+# 選択されたメニューのインスタンスを変数selected_menuに代入してください
+selected_menu = menus[order]
+
+# 「選択されたメニュー: ○○」となるように出力してください
+puts "選択されたメニュー: #{selected_menu.name}"
+
+puts "個数を入力してください(3つ以上で100円割引)"
+
+# 入力を数値として受け取って変数countに代入してください
+count = gets.chomp.to_i
+
+# 「お会計は○○円です」となるように出力してください
+puts "お会計は#{selected_menu.get_total_price(count)}円です"
