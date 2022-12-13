ffd46cfa0207acc44a34f00a1e71db7a31d25e59
keishimushup <108566690+keishimushup@users.noreply.github.com>
2022-12-13 07:58:15 +0000

    10 オーバーライド復習

--- index.rb
+++ index.rb
@@ -1,17 +1,14 @@
-
 require "./food"
 require "./drink"
 
-# Food.newの引数を書き換えてください
 food1 = Food.new(name: "ピザ", price: 800, calorie: 700)
-
 food2 = Food.new(name: "すし", price: 1000, calorie: 600)
 
+# Drink.newの引数を書き換えてください
+drink1 = Drink.new(name: "コーラ", price: 300, amount: 500)
 
-drink1 = Drink.new(name: "コーラ", price: 300)
-drink1.amount = 500
-drink2 = Drink.new(name: "お茶", price: 200)
-drink2.amount = 400
+drink2 = Drink.new(name: "お茶", price: 200, amount: 400)
+
 
 menus = [food1, food2, drink1, drink2]
 
