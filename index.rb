555e1823dc19ffa7f939528a979c53100ab086c7
keishimushup <108566690+keishimushup@users.noreply.github.com>
2022-12-13 07:14:18 +0000

    4 インスタンス変数の追加

--- index.rb
+++ index.rb
@@ -1,34 +1,17 @@
-require "./menu"
+require "./food"
+require "./drink"
 
-menu1 = Menu.new(name: "ピザ", price: 800)
-menu2 = Menu.new(name: "すし", price: 1000)
-menu3 = Menu.new(name: "コーラ", price: 300)
-menu4 = Menu.new(name: "お茶", price: 200)
+food1 = Food.new(name: "ピザ", price: 800)
+drink1 = Drink.new(name: "コーラ", price: 300)
 
-menus = [menu1, menu2, menu3, menu4]
+# food1.calorieに「700」を代入してください
+food1.calorie = 700
 
-index = 0
-menus.each do |menu|
-  puts "#{index}. #{menu.info}"
-  index += 1
-end
+# food1.calorieを出力してください
+puts food1.calorie
 
-puts "--------------"
-puts "メニューの番号を選択してください"
+# drink1.amountに「500」を代入してください
+drink1.amount = 500
 
-# 入力を数値として受け取って変数orderに代入してください
-order = gets.chomp.to_i
-
-# 選択されたメニューのインスタンスを変数selected_menuに代入してください
-selected_menu = menus[order]
-
-# 「選択されたメニュー: ○○」となるように出力してください
-puts "選択されたメニュー: #{selected_menu.name}"
-
-puts "個数を入力してください(3つ以上で100円割引)"
-
-# 入力を数値として受け取って変数countに代入してください
-count = gets.chomp.to_i
-
-# 「お会計は○○円です」となるように出力してください
-puts "お会計は#{selected_menu.get_total_price(count)}円です"
+# drink1.amountを出力してください
+puts drink1.amount
